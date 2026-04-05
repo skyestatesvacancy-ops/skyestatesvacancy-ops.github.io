53871fce8b4a9f2dab3c0024386245094e90d543
Omar <skyestatesvacancy@gmail.com>
2026-04-05 16:30:41 +0200

    Fix chat panel state reset - pause auto-refresh while modal open

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "propertyoro-v11";
+const CACHE = "propertyoro-v12";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", e => {
