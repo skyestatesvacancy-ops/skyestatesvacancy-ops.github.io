1ddb0f6941b1e15be8076040f79a79af7cd4f274
Omar <skyestatesvacancy@gmail.com>
2026-04-05 16:33:59 +0200

    Add debug info to chat loading for phone troubleshooting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "propertyoro-v12";
+const CACHE = "propertyoro-v13";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", e => {
