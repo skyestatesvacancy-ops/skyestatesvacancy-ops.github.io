7dd828900cf455f1124e0f22d4d29e6f81d6091b
Omar <skyestatesvacancy@gmail.com>
2026-04-05 14:53:50 +0200

    Bump cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "propertyoro-v3";
+const CACHE = "propertyoro-v4";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", e => {
