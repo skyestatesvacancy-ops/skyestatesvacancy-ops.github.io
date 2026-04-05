beab332cd592d1f2791e6a49fd8c9973416e963d
Omar <skyestatesvacancy@gmail.com>
2026-04-05 16:36:46 +0200

    Add persistent yellow debug box to chat panel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "propertyoro-v13";
+const CACHE = "propertyoro-v14";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", e => {
