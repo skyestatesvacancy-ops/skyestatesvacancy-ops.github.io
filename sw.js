79350c0ba95436d65a0370f12bfcbb163c6892c0
Omar <skyestatesvacancy@gmail.com>
2026-04-05 14:36:42 +0200

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "propertyoro-v2";
+const CACHE = "propertyoro-v3";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", e => {
